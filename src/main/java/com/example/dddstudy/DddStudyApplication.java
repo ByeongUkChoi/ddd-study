1bfd3f60ce6cf383263e2852aea6a93704bf1a29
ByeongUk Choi <cbw0916@gmail.com>
2021-02-27 21:07:57 +0900

    first commit

---
+++ src/main/java/com/example/dddstudy/DddStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddStudyApplication.class, args);
+    }
+
+}
