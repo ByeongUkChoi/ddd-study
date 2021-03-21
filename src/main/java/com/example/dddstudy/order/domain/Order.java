0f0098e77aaa6abbffa4e70dc405b12cebfc73cf
ByeongUk Choi <cbw0916@gmail.com>
2021-03-22 00:36:13 +0900

    Add validation

--- src/main/java/com/example/dddstudy/order/domain/Order.java
+++ src/main/java/com/example/dddstudy/order/domain/Order.java
@@ -8,14 +8,25 @@ public class Order {
     private DeliveryInfo deliveryInfo;
     private Status status;
 
+    /** 주문 생성 */
     public void order(Orderer orderer, List<OrderItem> orderItems, DeliveryInfo deliveryInfo) {
+        this.orderer = orderer;
+        this.orderItems = orderItems;
+        this.deliveryInfo = deliveryInfo;
+        this.status = Status.WAITING;     // <-- 주문 생성 시 대기중 상태로 생성됨
     }
 
+    /** 주문 취소 */
     public void cancel() {
-        if (Status.WAITING.equals(status)) {
+        if (enableOrderCancel()) {
             status = Status.CANCELED;
         }
         throw new RuntimeException();
+    }
+
+    /** 주문 취소 가능 여부 */
+    private boolean enableOrderCancel() {
+        return Status.WAITING.equals(status);
     }
 
     enum Status {
