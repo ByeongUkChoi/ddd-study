9cd0add92fe7f5da873b233a07554c12b983ab92
ByeongUk Choi <cbw0916@gmail.com>
2021-03-29 22:12:48 +0900

    Fix product domain

--- src/main/java/com/example/dddstudy/product/domain/Product.java
+++ src/main/java/com/example/dddstudy/product/domain/Product.java
@@ -18,18 +18,10 @@ public class Product {
 
     /** 주문 가능하게 설정 */
     public void changeOrderable(long storeId) {
-        validateUpdate(storeId);
         orderable = true;
     }
     /** 주문 불가능하게 설정 */
     public void changeUnorderable(long storeId) {
-        validateUpdate(storeId);
         orderable = false;
-    }
-    /** 변경 유효성 검사 */
-    private void validateUpdate(long storeId) {
-        if (this.storeId != storeId) {
-            throw new RuntimeException();
-        }
     }
 }
