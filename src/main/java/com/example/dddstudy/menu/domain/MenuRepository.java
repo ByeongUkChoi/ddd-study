4cefbfe6e5c3fbc0f16ab8d38fb7ff24a5257aa1
ByeongUk Choi <cbw0916@gmail.com>
2021-07-18 00:25:33 +0900

    Fix repository

--- src/main/java/com/example/dddstudy/menu/domain/MenuRepository.java
+++ src/main/java/com/example/dddstudy/menu/domain/MenuRepository.java
@@ -1,9 +1,11 @@
 package com.example.dddstudy.menu.domain;
 
+import org.springframework.data.repository.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface MenuRepository {
+public interface MenuRepository extends Repository<Menu, Long> {
     Optional<Menu> findById(long id);
     List<Menu> findAllById(Iterable<Long> ids);
 }
